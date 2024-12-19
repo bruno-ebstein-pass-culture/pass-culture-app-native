bce7bbb8a95a499e88482df9a102c08baf2afb93
bebstein-pass <95220086+bebstein-pass@users.noreply.github.com>
2024-12-19 15:15:38 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
   inputs.brew-nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.brew-nix.inputs.flake-utils.follows = "flake-utils";
 
+  inputs.nixcasks.url = "github:jacekszymanski/nixcasks";
+  inputs.nixcasks.inputs.nixpkgs.follows = "nixpkgs";
+
+
   inputs.mac-app-util.url = "github:hraban/mac-app-util";
 
   outputs =
@@ -17,21 +21,42 @@
     , flake-utils
     , brew-nix
     , mac-app-util
+    , nixcasks
     , ...
     }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShells.default =
         let
+          osVersion = "sonoma";
+
+          nixcasks_ = (nixcasks.output {
+            inherit osVersion;
+          }).packages.${system};
+
+          nixcasksOverlay = final: prev: {
+            nixcasks = (nixcasks.output {
+              inherit osVersion;
+            }).packages."${prev.system}";
+          };
+
+
           pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
             overlays = [
               brew-nix.overlays.default
+              nixcasksOverlay
             ];
+            # config.packageOverrides = _: {
+            #   nixcasks = nixcasks_;
+            #   # inherit nixcasks_;
+            # };
           };
           brewPackages = [
-            pkgs.brewCasks.android-studio
+            pkgs.nixcasks.android-studio
+            # pkgs.brewCasks.android-studio
             pkgs.brewCasks.firefox
+            # pkgs.nixcasks.firefox
           ];
           macAppUtil = pkgs.lib.getExe' mac-app-util.packages."${system}".default "mac-app-util";
           brewApplications = pkgs.runCommand "brewApplications" { } ''
@@ -52,12 +77,14 @@
             brewPackages
           ]);
 
-          shellHook = ''
-            # make apps available in the launcher and up to date in the dock
-            if [ "$(${pkgs.lib.getExe' pkgs.coreutils "uname"})" == "Darwin" ]; then
-              ${macAppUtil} sync-trampolines ${brewApplications} ~/Applications/pass-culture-app-native/
-            fi
-          '';
+          # shellHook = ''
+          #   # make apps available in the launcher and up to date in the dock
+          #   ls -l ${brewApplications}
+
+          #   if [ "$(${pkgs.lib.getExe' pkgs.coreutils "uname"})" == "Darwin" ]; then
+          #     ${macAppUtil} sync-trampolines ${brewApplications} ~/Applications/pass-culture-app-native/
+          #   fi
+          # '';
         };
     });
 }
